a53c1ddcbcde76f0d608fc4371102a4b73c6b1f4
hoolies <c.rouvellas@gmail.com>
2023-10-05 13:27:56 +0000

    Revert changes

--- uc-cli.py
+++ uc-cli.py
@@ -14,7 +14,6 @@ parser = ArgumentParser(
 # Set the optional parameters
 parser.add_argument('-c', '--convert',default = False, required = True, help = "Convert file in type.\nExample: \nuc-cli.py -c ./test.csv -t json")
 parser.add_argument('-t', '--type',default = False, required = True, help = "Convert file in type.\nExample: \nuc-cli.py -c ./test.csv -t xlsx")
-parser.add_argument('-r', '--rows',default = False, required = True, help = "Convert file in type.\nExample: \nuc-cli.py -c ./test.csv -t xlsx -r")
 
 # Parse the arguments
 args = parser.parse_args()
@@ -22,7 +21,6 @@ args = parser.parse_args()
 # Set variables with the arguments
 convert =  args.convert
 file_type = args.type
-rows =  args.rows
 
 
 # Select the file
@@ -86,23 +84,6 @@ def write_file(df,filename,file_type):
         print(e)
         return None
 
-# Write rows 
-def write_rows(df,filename,file_type):
-    try:
-        if file_type.lower() == 'csv':
-            fileout = sub(r'\.\w+', 'csv', filename)
-            df.write_csv(fileout, row_oriented=True)
-        elif file_type.lower() == 'xlsx':
-            fileout = sub(r'\.\w+', 'xlsx', filename)
-            df.write_excel(fileout, row_oriented=True)
-        elif file_type.lower() == 'json':
-            fileout = sub(r'(?<=\.)\w+', 'json', filename)
-            df.write_json(fileout, row_oriented=True)
-        else:
-            print("Error, please enter a valid format: csv,xlsx or json")
-    except ValueError as e:
-        print(e)
-        return None
 
 
 # Main Function
@@ -111,10 +92,7 @@ def main():
         filename = cherry_pick(convert)
         format = check_format(filename) # type: ignore
         df = read_file(filename,format)
-        if rows:
-            write_rows(df,filename,file_type)
-        else:
-            write_file(df,filename,file_type)
+        write_file(df,filename,file_type)
 
 
 if __name__ == "__main__":
