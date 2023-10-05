b31c61d16eca9fb2bb46d1d36396eb916bb69b51
hoolies <c.rouvellas@gmail.com>
2023-10-05 15:23:11 +0000

    92

--- 92/lrnimport/intscript02.py
+++ 92/lrnimport/intscript02.py
@@ -6,3 +6,8 @@ interface = input("Enter an interface, like, ens3: ")
 subprocess.call(["ip", "addr", "show", "dev", interface])  ## <--- changed
 subprocess.call(["ip", "route", "show", "dev", interface]) ## <--- changed
 
+choice = input("Would you like to run Wireshark on the interface?")
+
+if choise.startswith("y"):
+    subprocess.call(["tshark", "-i", f"{interface}"]
+
