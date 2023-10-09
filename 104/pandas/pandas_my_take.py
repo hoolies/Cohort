2a74d431f93632015a46f2dbbc518fca752d03d7
hoolies <c.rouvellas@gmail.com>
2023-10-09 15:28:42 +0000

    Pandas better version

---
+++ 104/pandas/pandas_my_take.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This is a script to convert json,csv and xlsx"""
+
+import pandas as pd
+
+def read_file() -> pd.DataFrame: # type: ignore
+    """This function reads the file"""
+    filename = input("Enter the file name: ")
+    if filename.endswith(".json"):
+        return pd.read_json(filename),filename # type: ignore
+    elif filename.endswith(".csv"):
+        return pd.read_csv(filename),filename # type: ignore
+    elif filename.endswith(".xlsx"):
+        return pd.read_excel(filename),filename # type: ignore
+    else:
+        print("File format not supported")
+
+def write_file(filename,df) -> None:
+    """This function writes the file"""
+    extension = input("Enter the file type that you would like to convert to: ").lower()
+    if extension=="json":
+        df.to_json(filename)
+    elif extension=="csv":
+        df.to_csv(filename)
+    elif extension=="xlsx":
+        df.to_excel(filename)
+    else:
+        print("File format not supported")
+
+def main():
+    """Main Function"""
+    df,filename=read_file()
+    write_file(filename,df)
+
+
+if __name__ == "__main__":
+    main()
+
