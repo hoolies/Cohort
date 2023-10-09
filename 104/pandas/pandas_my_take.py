19c95389fb07bb2a836921592aabb6ed3d04b275
hoolies <c.rouvellas@gmail.com>
2023-10-09 15:47:23 +0000

    Fix extension on output

--- 104/pandas/pandas_my_take.py
+++ 104/pandas/pandas_my_take.py
@@ -18,12 +18,13 @@ def read_file() -> pd.DataFrame: # type: ignore
 def write_file(filename,df) -> None:
     """This function writes the file"""
     extension = input("Enter the file type that you would like to convert to: ").lower()
+    fileout =filename.split(".")[0] + "." + extension
     if extension=="json":
-        df.to_json(filename)
+        df.to_json(fileout)
     elif extension=="csv":
-        df.to_csv(filename)
+        df.to_csv(fileout)
     elif extension=="xlsx":
-        df.to_excel(filename)
+        df.to_excel(fileout)
     else:
         print("File format not supported")
 
