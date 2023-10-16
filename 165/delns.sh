629a1691354a6c2962718d3acdc5e8604468097e
hoolies <c.rouvellas@gmail.com>
2023-10-16 16:02:51 +0000

    Adding and removind net namespaces

---
+++ 165/delns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo ip netns delete ohost
+sudo ip netns delete phost    
+sudo ip netns delete whost
+sudo ip netns delete yhost
+sudo ip netns delete prouter
+sudo ip netns delete yrouter
+sudo ip netns delete wrouter
+sudo ip netns delete orouter
+sudo ip netns delete crouter
